c75360effbc666f78ba802496e3cf029e6554c7c
[200~Blaze-J <anhtgcs18069@fpt.edu.vn>
2023-12-12 01:07:51 +0700

    Update V3

--- FPTBook-V2/FPTBook/FPTBook/Models/ChangePass.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace FPTBook.Models
-{
-    public class ChangePass
-    {
-        public string Id { get; set; }
-        public string NewPass { get; set; }
-        public string ConfirmNewPass { get; set; }
-    }
-}
