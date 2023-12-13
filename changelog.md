7c231f9136dfd73aeddf038b4861a973ea87ac95
[200~Blaze-J <anhtgcs18069@fpt.edu.vn>
2023-12-14 01:22:32 +0700

    Update V5 fix bug

--- changelog.md
+++
@@ -1,3 +0,0 @@
-Update FptBook V2 add funtion Refresh Cart " Update more Product for customer "
-Update FptBook V3 add more product and fix some error
-Update FptBook V4 add button address and phone when summit cart
---
+++ changelog.py
@@ -0,0 +1,7 @@
+"Update FptBook V2 add funtion Refresh Cart " Update more Product for customer "
+
+-- Update FptBook V3 add more product and fix some error
+
+-- Update FptBook V4 add button address and phone when summit cart
+
+ # Update FptBook V5 fix error when summit cart don't trigger DB and count amount book change money
